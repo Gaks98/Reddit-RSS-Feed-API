a6a9d5d516e977aadb6f5ce9225b020fde1fad47
Gaks98 <yvonnegax98@gmail.com>
2019-10-08 16:39:22 +0300

    Arraylist to get each post

--- app/src/main/java/com/moringaschool/redditrssapi/MainActivity.java
+++ app/src/main/java/com/moringaschool/redditrssapi/MainActivity.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.moringaschool.redditrssapi.model.Feed;
 import com.moringaschool.redditrssapi.model.entry.Entry;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Call;
@@ -53,6 +55,47 @@ public class MainActivity extends AppCompatActivity {
 //                Log.d(TAG, "onResponse: updated: " + entrys.get(0).getUpdated());
 //                Log.d(TAG, "onResponse: title: " + entrys.get(0).getTitle());
 
+                ArrayList<Post> posts = new ArrayList<Post>();
+                for (int i = 0; i< entrys.size(); i++){
+                    ExtractXML extractXML1 = new ExtractXML(entrys.get(i).getContent(), "<a href=");
+                    List<String> postContent = extractXML1.start();
+
+
+                    ExtractXML extractXML2 = new ExtractXML(entrys.get(i).getContent(), "<img src=");
+
+                    try{
+                        postContent.add(extractXML2.start().get(0));
+                    }catch (NullPointerException e){
+                        postContent.add(null);
+                        Log.e(TAG, "onResponse: NullPointerException(thumbnail):" + e.getMessage() );
+                    }
+                    catch (IndexOutOfBoundsException e){
+                        postContent.add(null);
+                        Log.e(TAG, "onResponse: IndexOutOfBoundsException(thumbnail):" + e.getMessage() );
+                    }
+
+                    int lastPosition = postContent.size() - 1;
+                    posts.add(new Post(
+                            entrys.get(i).getTitle(),
+                            entrys.get(i).getAuthor().getName(),
+                            entrys.get(i).getUpdated(),
+                            postContent.get(0),
+                            postContent.get(lastPosition)
+                    ));
+                }
+
+                for(int j = 0; j<posts.size(); j++){
+                    Log.d(TAG, "onResponse: \n " +
+                            "PostURL: " + posts.get(j).getPostURL() + "\n " +
+                            "ThumbnailURL: " + posts.get(j).getThumbnailURL() + "\n " +
+                            "Title: " + posts.get(j).getTitle() + "\n " +
+                            "Author: " + posts.get(j).getAuthor() + "\n " +
+                            "updated: " + posts.get(j).getDate_updated() + "\n ");
+                }
+
+//                ListView listView = (ListView) findViewById(R.id.listView);
+//                CustomListAdapter customListAdapter = new CustomListAdapter(MainActivity.this, R.layout.card_layout_main, posts);
+//                listView.setAdapter(customListAdapter);
             }
 
             @Override
