fcc700352b69d70b9cc3f268d388cad30bf82a2d
Gaks98 <yvonnegax98@gmail.com>
2019-10-08 20:09:09 +0300

    Intoduced the non-static @Get method and commented out the static one

--- app/src/main/java/com/moringaschool/redditrssapi/FeedApi.java
+++ app/src/main/java/com/moringaschool/redditrssapi/FeedApi.java
@@ -4,12 +4,19 @@ import com.moringaschool.redditrssapi.model.Feed;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public interface FeedApi {
     String BASE_URL = "https://www.reddit.com/r/";
 
-    @GET("earthporn/.rss")
-    Call<Feed> getFeed();
+    //Non-static feed name
+    @GET("{feed_name}/.rss")
+    Call<Feed> getFeed(@Path("feed_name") String feed_name);
+
+
+//    //static
+//    @GET("earthporn/.rss")
+//    Call<Feed> getFeed();
 
 
 
