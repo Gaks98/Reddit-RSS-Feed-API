1ee32ee9e491b32381b8c2423d05e4883dd33554
Gaks98 <yvonnegax98@gmail.com>
2019-10-08 13:58:02 +0300

    generated a contructor + getters and setters

--- app/src/main/java/com/moringaschool/redditrssapi/model/entry/Entry.java
+++ app/src/main/java/com/moringaschool/redditrssapi/model/entry/Entry.java
@@ -24,6 +24,54 @@ public class Entry implements Serializable {
     @Element(name = "updated")
     private String updated;
 
+    public Entry() {
 
+    }
 
+    public Entry(String content, String author, String title, String updated) {
+        this.content = content;
+        this.author = author;
+        this.title = title;
+        this.updated = updated;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(String updated) {
+        this.updated = updated;
+    }
 }
