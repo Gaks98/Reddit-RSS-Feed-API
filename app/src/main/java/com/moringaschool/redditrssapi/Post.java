9a42b3863ebdcca98dc20f7e54d34c2d9da2842f
Gaks98 <yvonnegax98@gmail.com>
2019-10-08 16:32:33 +0300

    generated default constructor

--- app/src/main/java/com/moringaschool/redditrssapi/Post.java
+++ app/src/main/java/com/moringaschool/redditrssapi/Post.java
@@ -6,4 +6,12 @@ public class Post {
     private String date_updated;
     private String postURL;
     private String thumbnailURL;
+
+    public Post(String title, String author, String date_updated, String postURL, String thumbnailURL) {
+        this.title = title;
+        this.author = author;
+        this.date_updated = date_updated;
+        this.postURL = postURL;
+        this.thumbnailURL = thumbnailURL;
+    }
 }
