532c14c69e4b546506ed7566534b770bb0a97ea7
Gaks98 <yvonnegax98@gmail.com>
2019-10-08 16:31:13 +0300

    created all the fields we are going to be getting

--- app/src/main/java/com/moringaschool/redditrssapi/Post.java
+++ app/src/main/java/com/moringaschool/redditrssapi/Post.java
@@ -1,4 +1,9 @@
 package com.moringaschool.redditrssapi;
 
 public class Post {
+    private String title;
+    private String author;
+    private String date_updated;
+    private String postURL;
+    private String thumbnailURL;
 }
