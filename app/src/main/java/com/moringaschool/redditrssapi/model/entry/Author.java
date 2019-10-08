2fd3ff3ee188e3a2156a128e71b9aa7b55ae1b5e
Gaks98 <yvonnegax98@gmail.com>
2019-10-08 14:03:24 +0300

    created two tags name and uri

--- app/src/main/java/com/moringaschool/redditrssapi/model/entry/Author.java
+++ app/src/main/java/com/moringaschool/redditrssapi/model/entry/Author.java
@@ -1,9 +1,16 @@
 package com.moringaschool.redditrssapi.model.entry;
 
+import org.simpleframework.xml.Element;
 import org.simpleframework.xml.Root;
 
 import java.io.Serializable;
 
 @Root(name = "entry",strict = false)
 public class Author implements Serializable {
+
+    @Element(name = "name")
+    private String name;
+
+    @Element(name = "uri")
+    private String uri;
 }
