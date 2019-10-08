326e17f7386927c13e5c99a9431663edc996f1d2
Gaks98 <yvonnegax98@gmail.com>
2019-10-08 14:10:39 +0300

    generated the retrofit class in MainActivity and alsop added the BASE_URL string

--- app/src/main/java/com/moringaschool/redditrssapi/MainActivity.java
+++ app/src/main/java/com/moringaschool/redditrssapi/MainActivity.java
@@ -4,11 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import retrofit2.Retrofit;
+
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    private static final String BASE_URL = "https://www.reddit.com/r/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .build();
+
+        FeedApi feedApi = retrofit.create(FeedApi.class);
     }
 }
