e0361f2defdc9666ba3d5af8cfaa7e34c4125e6c
Gaks98 <yvonnegax98@gmail.com>
2019-10-08 15:39:25 +0300

    created a start() that will return a list of the parsed in data

--- app/src/main/java/com/moringaschool/redditrssapi/ExtractXML.java
+++ app/src/main/java/com/moringaschool/redditrssapi/ExtractXML.java
@@ -1,9 +1,16 @@
 package com.moringaschool.redditrssapi;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ExtractXML {
 
     private static final String TAG = "ExtractXML";
 
     private String tag;
     private String xml;
+
+    public List<String> start() {
+        List<String> result = new ArrayList<>();
+    }
 }
