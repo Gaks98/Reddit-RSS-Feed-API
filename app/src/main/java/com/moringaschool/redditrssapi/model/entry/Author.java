2e5f26617326f67eea2c5ab3421218c58144679c
Gaks98 <yvonnegax98@gmail.com>
2019-10-08 14:02:12 +0300

    created the @Root()

--- app/src/main/java/com/moringaschool/redditrssapi/model/entry/Author.java
+++ app/src/main/java/com/moringaschool/redditrssapi/model/entry/Author.java
@@ -1,6 +1,9 @@
 package com.moringaschool.redditrssapi.model.entry;
 
+import org.simpleframework.xml.Root;
+
 import java.io.Serializable;
 
+@Root(name = "entry",strict = false)
 public class Author implements Serializable {
 }
