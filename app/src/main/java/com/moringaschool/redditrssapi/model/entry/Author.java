fe487daa45fd77d248f6917daf1b24fc2a0a082f
Gaks98 <yvonnegax98@gmail.com>
2019-10-08 14:04:07 +0300

    created getters and setters

--- app/src/main/java/com/moringaschool/redditrssapi/model/entry/Author.java
+++ app/src/main/java/com/moringaschool/redditrssapi/model/entry/Author.java
@@ -13,4 +13,22 @@ public class Author implements Serializable {
 
     @Element(name = "uri")
     private String uri;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUri() {
+        return uri;
+    }
+
+    public void setUri(String uri) {
+        this.uri = uri;
+    }
+
+
 }
