a90416ca22e88e3d1e458d361b848e45e5c63d74
Gaks98 <yvonnegax98@gmail.com>
2019-10-08 14:17:05 +0300

    added an Xml coverter method though it's deprecated meaning it's dead but still fuctional

--- app/src/main/java/com/moringaschool/redditrssapi/MainActivity.java
+++ app/src/main/java/com/moringaschool/redditrssapi/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import retrofit2.Retrofit;
+import retrofit2.converter.simplexml.SimpleXmlConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -18,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
+                .addConverterFactory(SimpleXmlConverterFactory.create())
                 .build();
 
         FeedApi feedApi = retrofit.create(FeedApi.class);
