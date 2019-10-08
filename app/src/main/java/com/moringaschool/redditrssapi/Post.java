46a7b4e2e2007029bdea6957222908e373581250
Gaks98 <yvonnegax98@gmail.com>
2019-10-08 16:34:01 +0300

    generated getters and setters

--- app/src/main/java/com/moringaschool/redditrssapi/Post.java
+++ app/src/main/java/com/moringaschool/redditrssapi/Post.java
@@ -14,4 +14,45 @@ public class Post {
         this.postURL = postURL;
         this.thumbnailURL = thumbnailURL;
     }
+        public String getTitle () {
+            return title;
+        }
+
+        public void setTitle (String title){
+            this.title = title;
+        }
+
+        public String getAuthor () {
+            return author;
+        }
+
+        public void setAuthor (String author){
+            this.author = author;
+        }
+
+        public String getDate_updated () {
+            return date_updated;
+        }
+
+        public void setDate_updated (String date_updated){
+            this.date_updated = date_updated;
+        }
+
+        public String getPostURL () {
+            return postURL;
+        }
+
+        public void setPostURL (String postURL){
+            this.postURL = postURL;
+        }
+
+        public String getThumbnailURL () {
+            return thumbnailURL;
+        }
+
+        public void setThumbnailURL (String thumbnailURL){
+            this.thumbnailURL = thumbnailURL;
+        }
 }
+
+
