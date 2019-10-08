39d23ff0bec14db765d6ed89ea62e76d5ff3a632
Gaks98 <yvonnegax98@gmail.com>
2019-10-08 13:39:34 +0300

    added an @Root method that allows our app to grab the data even if we've not included some of the tags parent components

--- app/src/main/java/com/moringaschool/redditrssapi/model/Feed.java
+++ app/src/main/java/com/moringaschool/redditrssapi/model/Feed.java
@@ -1,4 +1,30 @@
 package com.moringaschool.redditrssapi.model;
 
-public class Feed {
-}
+import org.simpleframework.xml.Element;
+import org.simpleframework.xml.Root;
+
+import java.io.Serializable;
+
+/
+@Root(name = "feed", strict = false)
+public class Feed implements Serializable {
+
+    //tags I am going to extract from the reddit feed
+    @Element(name = "icon")
+    private String icon;
+
+    @Element(name = "id")
+    private String id;
+
+    @Element(name = "logo")
+    private String logo;
+
+    @Element(name = "title")
+    private String title;
+
+    @Element(name = "updated")
+    private String updated;
+
+    @Element(name = "subtitle")
+    private String subtitle;
+
