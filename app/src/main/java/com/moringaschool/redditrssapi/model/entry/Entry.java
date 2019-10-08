4e3cba797151b0e5a4b8b248033b343988670243
Gaks98 <yvonnegax98@gmail.com>
2019-10-08 13:54:26 +0300

    added the tags we are going to grab as instance variables

--- app/src/main/java/com/moringaschool/redditrssapi/model/entry/Entry.java
+++ app/src/main/java/com/moringaschool/redditrssapi/model/entry/Entry.java
@@ -1,5 +1,6 @@
 package com.moringaschool.redditrssapi.model.entry;
 
+import org.simpleframework.xml.Element;
 import org.simpleframework.xml.Root;
 import org.simpleframework.xml.Serializer;
 
@@ -7,4 +8,22 @@ import java.io.Serializable;
 
 @Root(name = "entry",strict = false)
 public class Entry implements Serializable {
+
+    @Element(name = "content")
+    private String content;
+
+    @Element(required = false,name = "author")
+    private String author;
+
+    @Element(name = "id")
+    private String id;
+
+    @Element(name = "title")
+    private String title;
+
+    @Element(name = "updated")
+    private String updated;
+
+
+
 }
