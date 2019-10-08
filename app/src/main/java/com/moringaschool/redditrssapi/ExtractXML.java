1a37569f49ce153ff43c0567383914426e8ca5da
Gaks98 <yvonnegax98@gmail.com>
2019-10-08 15:43:39 +0300

    created a String array that will split the tags and then iterate thru if their is more than one of the same tag

--- app/src/main/java/com/moringaschool/redditrssapi/ExtractXML.java
+++ app/src/main/java/com/moringaschool/redditrssapi/ExtractXML.java
@@ -12,5 +12,21 @@ public class ExtractXML {
 
     public List<String> start() {
         List<String> result = new ArrayList<>();
+
+        String[] splitXML = xml.split(tag + "\"");
+        int count = splitXML.length;
+
+        for( int i = 1; i < count; i++){
+            String temp = splitXML[i];
+            int index = temp.indexOf("\"");
+            //Log.d(TAG, "start: index: " + index);
+            //Log.d(TAG, "start: extracted: " + temp);
+
+            temp = temp.substring(0,index);
+            //Log.d(TAG, "start: snipped: " + temp);
+            result.add(temp);
+        }
+
+        return result;
     }
 }
