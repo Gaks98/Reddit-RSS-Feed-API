74294e38d5ac8519d4952e10baee3abc8ca636a6
Gaks98 <yvonnegax98@gmail.com>
2019-10-08 13:46:07 +0300

    added getter and setters for all the tags and also added a new tag-entry tag

--- app/src/main/java/com/moringaschool/redditrssapi/model/Feed.java
+++ app/src/main/java/com/moringaschool/redditrssapi/model/Feed.java
@@ -1,11 +1,16 @@
 package com.moringaschool.redditrssapi.model;
 
+import com.moringaschool.redditrssapi.model.entry.Entry;
+
 import org.simpleframework.xml.Element;
+import org.simpleframework.xml.ElementList;
 import org.simpleframework.xml.Root;
+//import org.simpleframework.xml.util.Entry;
 
 import java.io.Serializable;
+import java.util.List;
 
-/
+
 @Root(name = "feed", strict = false)
 public class Feed implements Serializable {
 
@@ -27,4 +32,64 @@ public class Feed implements Serializable {
 
     @Element(name = "subtitle")
     private String subtitle;
+
+    @ElementList(inline = true, name = "entry")
+    private List<Entry> entrys;
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getLogo() {
+        return logo;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getUpdated() {
+        return updated;
+    }
+
+    public String getSubtitle() {
+        return subtitle;
+    }
+
+    public List<Entry> getEntrys() {
+        return entrys;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setLogo(String logo) {
+        this.logo = logo;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setUpdated(String updated) {
+        this.updated = updated;
+    }
+
+    public void setSubtitle(String subtitle) {
+        this.subtitle = subtitle;
+    }
+
+    public void setEntrys(List<Entry> entrys) {
+        this.entrys = entrys;
+    }
+}
 
---
+++ app/src/main/java/com/moringaschool/redditrssapi/model/entry/Entry.java
@@ -0,0 +1,4 @@
+package com.moringaschool.redditrssapi.model.entry;
+
+public class Entry {
+}
