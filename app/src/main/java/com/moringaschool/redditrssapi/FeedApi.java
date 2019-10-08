8fc237bdadef1cc190cb61db030e4dbd1931eb50
Gaks98 <yvonnegax98@gmail.com>
2019-10-08 13:16:14 +0300

    added @Get method to fetch the field

--- app/src/main/java/com/moringaschool/redditrssapi/FeedApi.java
+++ app/src/main/java/com/moringaschool/redditrssapi/FeedApi.java
@@ -1,5 +1,15 @@
 package com.moringaschool.redditrssapi;
 
+import com.moringaschool.redditrssapi.model.Feed;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface FeedApi {
-    String Base_URL = "https://www.reddit.com/r/";
+    String BASE_URL = "https://www.reddit.com/r/";
+
+    @GET("earthporn/.rss")
+    Call<Feed> getFeed;
+
+
 }
---
+++ app/src/main/java/com/moringaschool/redditrssapi/model/Feed.java
@@ -0,0 +1,4 @@
+package com.moringaschool.redditrssapi.model;
+
+public class Feed {
+}
