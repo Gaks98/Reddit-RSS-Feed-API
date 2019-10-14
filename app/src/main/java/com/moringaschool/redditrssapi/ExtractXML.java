83c3e128eb5e6972b8193a7f620ddd636007d31f
Gaks98 <yvonnegax98@gmail.com>
2019-10-14 14:11:48 +0300

    overidded the constructor and added an extra property

--- app/src/main/java/com/moringaschool/redditrssapi/ExtractXML.java
+++ app/src/main/java/com/moringaschool/redditrssapi/ExtractXML.java
@@ -1,5 +1,7 @@
 package com.moringaschool.redditrssapi;
 
+import android.util.Log;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,27 +10,46 @@ public class ExtractXML {
     private static final String TAG = "ExtractXML";
 
     private String tag;
+    private String endtag;
     private String xml;
 
     public ExtractXML(String xml, String tag) {
         this.tag = tag;
         this.xml = xml;
+        this.endtag = "NONE";
     }
 
-    public List<String> start() {
-        List<String> result = new ArrayList<>();
+    public ExtractXML(String xml, String tag, String endtag) {
+        this.tag = tag;
+        this.xml = xml;
+        this.endtag = endtag;
+    }
 
-        String[] splitXML = xml.split(tag + "\"");
+    public List<String> start(){
+        List<String> result = new ArrayList<>();
+        String[] splitXML = null;
+        String marker = null;
+
+
+        if(endtag.equals("NONE")){
+            marker = "\"";
+            splitXML = xml.split(tag + marker);
+        }
+        else{
+            marker = endtag;
+            splitXML = xml.split(tag);
+        }
+
         int count = splitXML.length;
 
         for( int i = 1; i < count; i++){
             String temp = splitXML[i];
-            int index = temp.indexOf("\"");
-            //Log.d(TAG, "start: index: " + index);
-            //Log.d(TAG, "start: extracted: " + temp);
+            int index = temp.indexOf(marker);
+            Log.d(TAG, "start: index: " + index);
+            Log.d(TAG, "start: extracted: " + temp);
 
             temp = temp.substring(0,index);
-            //Log.d(TAG, "start: snipped: " + temp);
+            Log.d(TAG, "start: snipped: " + temp);
             result.add(temp);
         }
 
