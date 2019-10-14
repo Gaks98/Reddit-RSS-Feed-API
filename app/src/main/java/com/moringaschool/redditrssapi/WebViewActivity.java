f3295e8b904e59e40dd344234374217457853f8f
Gaks98 <yvonnegax98@gmail.com>
2019-10-14 15:41:31 +0300

    set properties for the webview,textview and progress bar

--- app/src/main/java/com/moringaschool/redditrssapi/WebViewActivity.java
+++ app/src/main/java/com/moringaschool/redditrssapi/WebViewActivity.java
@@ -1,6 +1,13 @@
 package com.moringaschool.redditrssapi;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +20,20 @@ public class WebViewActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.webview_layout);
+
+        WebView webview = (WebView) findViewById(R.id.webview);
+        final ProgressBar progressBar = (ProgressBar) findViewById(R.id.webviewLoadingProgressBar);
+        final TextView loadingText = (TextView) findViewById(R.id.progressText);
+        Log.d(TAG, "onCreate: Started.");
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        Intent intent = getIntent();
+        String url = intent.getStringExtra("url");
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.loadUrl(url);
+
+        webview.setWebViewClient(new WebViewClient(){
 
     }
 }
