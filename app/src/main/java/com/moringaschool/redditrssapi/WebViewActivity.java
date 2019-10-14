0571acf1fbaaa43d592b21b5fca6f13e23c8b36c
Gaks98 <yvonnegax98@gmail.com>
2019-10-14 15:38:53 +0300

    added an onCreate method with the setContent view pointing to the webview_layout

--- app/src/main/java/com/moringaschool/redditrssapi/WebViewActivity.java
+++ app/src/main/java/com/moringaschool/redditrssapi/WebViewActivity.java
@@ -1,8 +1,18 @@
 package com.moringaschool.redditrssapi;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class WebViewActivity extends AppCompatActivity {
 
     private static final String TAG = "WebViewActivity";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.webview_activity);
+
+    }
 }
