0b82c48531a549747aa187d2160ae17997502140
Gaks98 <yvonnegax98@gmail.com>
2019-10-14 15:36:48 +0300

    extended the class to AppCompatActivity and added a final property TAG

---
+++ app/src/main/java/com/moringaschool/redditrssapi/WebViewActivity.java
@@ -0,0 +1,8 @@
+package com.moringaschool.redditrssapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WebViewActivity extends AppCompatActivity {
+
+    private static final String TAG = "WebViewActivity";
+}
