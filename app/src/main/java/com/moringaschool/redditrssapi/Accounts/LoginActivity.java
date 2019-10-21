91d014e1bbe7e7a2d06f6fb68192dbe06fc9d874
Gaks98 <yvonnegax98@gmail.com>
2019-10-21 13:09:56 +0300

    added 3 fields for progressbar, username and password plus an onCreate method

--- app/src/main/java/com/moringaschool/redditrssapi/Accounts/LoginActivity.java
+++ app/src/main/java/com/moringaschool/redditrssapi/Accounts/LoginActivity.java
@@ -1,4 +1,51 @@
 package com.moringaschool.redditrssapi.Accounts;
 
-public class LoginActivity {
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.moringaschool.redditrssapi.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = "LoginActivity";
+
+    private ProgressBar mProgressBar;
+    private EditText mUsername;
+    private EditText mPassword;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Log.d(TAG, "onCreate: started.");
+        final Button btnLogin  = (Button) findViewById(R.id.btn_login);
+        mPassword = (EditText) findViewById(R.id.input_password);
+        mUsername = (EditText) findViewById(R.id.input_username);
+        mProgressBar = (ProgressBar) findViewById(R.id.loginRequestLoadingProgressBar);
+        mProgressBar.setVisibility(View.GONE);
+
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: Attempting to log in.");
+                String username = mUsername.getText().toString();
+                String password = mPassword.getText().toString();
+
+                if(!username.equals("") && !password.equals("")){
+                    mProgressBar.setVisibility(View.VISIBLE);
+                    //method for signing in
+                }
+            }
+        });
+
+    }
 }
