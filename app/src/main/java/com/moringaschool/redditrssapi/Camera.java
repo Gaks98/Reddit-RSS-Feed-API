ad49b6e9c7fe6dcd487c9b9c7be14837e61e933b
Gaks98 <yvonnegax98@gmail.com>
2019-10-27 18:49:41 +0300

    camera-permission feature

--- app/src/main/java/com/moringaschool/redditrssapi/Camera.java
+++ app/src/main/java/com/moringaschool/redditrssapi/Camera.java
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.moringaschool.redditrssapi.Fragments.CameraFragment;
+
 public class Camera extends AppCompatActivity {
 
     FrameLayout frameLayout;
