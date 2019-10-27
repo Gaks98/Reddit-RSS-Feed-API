93abfc149ba057fb51b6fee6e0191b9a805b631d
Gaks98 <yvonnegax98@gmail.com>
2019-10-27 17:36:03 +0300

    added framelayout

--- app/src/main/java/com/moringaschool/redditrssapi/Camera.java
+++ app/src/main/java/com/moringaschool/redditrssapi/Camera.java
@@ -1,4 +1,32 @@
 package com.moringaschool.redditrssapi;
 
-public class Camera {
+import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+public class Camera extends AppCompatActivity {
+
+    FrameLayout frameLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        loadFragment(new CameraFragment(), false);
+
+    }
+
+    public void loadFragment(Fragment fragment, Boolean bool) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frameLayout, fragment);
+        if (bool)
+            transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+
 }
